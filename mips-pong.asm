8453643b368002ca944f4a311b0d84f58143b3dd
DazedFury <jmtexas19@gmail.com>
2018-12-08 04:42:44 -0500

    feat: Draw first paddle

--- mips-pong.asm
+++ mips-pong.asm
@@ -8,7 +8,160 @@
        				.word   0 : 40
 	stack_end:
 	
+	#COLOR TABLE
+	colourTable:		.word	0x000000	#Black
+				.word	0x0000ff	#Blue
+				.word	0x00ff00	#Green
+				.word	0xff0000	#Red
+				.word	0x00ffff	#Blue-Green
+				.word	0xff00ff	#Blue-Red
+				.word	0xffff00	#Green-Red
+				.word	0xffffff	#White
+	
 	
 	
 .text
 main:
+	#STACK
+	la		$sp, stack_end
+	
+	#X and Y Coordinates = 0-63
+	#Color = 0-7
+	
+	#Draw Paddle 1
+	li		$a0, 1
+	li		$a1, 24
+	li		$a2, 7
+	li		$a3, 15
+	jal		drawVertLine
+
+	#EXIT
+	exit:
+	li		$v0, 17			#Load exit call
+	syscall					#Execute
+
+
+#Procedure: drawDot:
+#Draw a dot on the bitmap display
+#$a0 = x coordinate (0-31)
+#$a1 = y coordinate (0-31)	
+#$a2 = colour number (0-7)
+drawDot:
+	#MAKE ROOM ON STACK
+	addi		$sp, $sp, -8		#Make room on stack for 2 words
+	sw		$a2, 0($sp)		#Store $a2 on element 0 of stack
+	sw		$ra, 4($sp)		#Store $ra on element 1 of stack
+
+	
+	#CALCULATE ADDRESS
+	jal		calculateAddress	#returns address of pixel in $v0
+	lw		$a2, 0($sp)		#Restore $a2 from stack
+	sw		$v0, 0($sp)		#Save $v0 on element 0 of stack
+	
+	#GET COLOR
+	jal		getColour		#Returns colour in $v1
+	lw		$v0, 0($sp)		#Restores $v0 from stack
+	
+	#MAKE DOT AND RESTORE $RA
+	sw		$v1, 0($v0)		#Make dot
+	lw		$ra, 4($sp)		#Restore $ra from stack
+	addi		$sp, $sp, 8		#Readjust stack
+	
+	jr		$ra			#Return
+	
+#Procedure: calculateAddress:
+#Convert x and y coordinate to address
+#$a0 = x coordinate (0-31)
+#$a1 = y coordinate (0-31)
+#$v0 = memory address
+calculateAddress:
+	#CALCULATIONS
+	sll		$a0, $a0, 2		#Multiply $a0 by 4
+	sll		$a1, $a1, 5		#Multiply $a1 by 256
+	sll		$a1, $a1, 3		#Multiply $a1 by 4
+	add		$a0, $a0, $a1		#Add $a1 to $a0
+	addi		$v0, $a0, 0x10040000	#Add base address for display + $a0 to $v0
+	
+	jr		$ra			#Return
+	
+#Procedure: getColour:
+#Get the colour based on $a2
+#$a2 = colour number (0-7)
+getColour:
+	#GET COLOUR	
+	la		$a0, colourTable	#Load Base
+	sll		$a2, $a2, 2		#Index x4 is offset
+	add		$a2, $a2, $a0		#Address is base + offset
+	lw		$v1, 0($a2)		#Get actual color from memory
+
+	jr		$ra			#Return
+	
+#Procedure: drawHorzLine:
+#Draw a horizontal line on the bitmap display
+#$a0 = x coordinate (0-31)
+#$a1 = y coordinate (0-31)
+#$a2 = colour number (0-7)
+#$a3 = length of the line
+drawHorzLine:
+	#MAKE ROOM ON STACK AND SAVE REGISTERS
+	addi		$sp, $sp, -16		#Make room on stack for 4 words
+	sw		$ra, 12($sp)		#Store $ra on element 4 of stack
+	sw		$a0, 0($sp)		#Store $a0 on element 0 of stack
+	sw		$a1, 4($sp)		#Store $a1 on element 1 of stack
+	sw		$a2, 8($sp)		#Store $a2 on element 2 of stack
+	
+	#HORIZONTAL LOOP
+	horzLoop:
+	jal		drawDot			#Jump and Link to drawDot
+	
+	#RESTORE REGISTERS
+	lw		$a0, 0($sp)		#Restore $a0 from stack
+	lw		$a1, 4($sp)		#Restore $a1 from stack
+	lw		$a2, 8($sp)		#Restore $a2 from stack
+	
+	#INCREMENT VALUES
+	addi		$a0, $a0, 1		#Increment x by 1
+	sw		$a0, 0($sp)		#Store $a0 on element 0 of stack
+	addi		$a3, $a3, -1		#Decrement length of line
+	bne		$a3, $0, horzLoop	#If length is not 0, loop
+	
+	#RESTORE $RA
+	lw		$ra, 12($sp)		#Restore $ra from stack
+	addi		$sp, $sp, 16		#Readjust stack
+	
+	jr		$ra			#Return
+	
+#Procedure: drawVertLine:
+#Draw a vertical line on the bitmap display
+#$a0 = x coordinate (0-31)
+#$a1 = y coordinate (0-31)
+#$a2 = colour number (0-7)
+#$a3 = length of the line (1-32)
+drawVertLine:
+	#MAKE ROOM ON STACK AND SAVE REGISTERS
+	addi		$sp, $sp, -16		#Make room on stack for 4 words
+	sw		$ra, 12($sp)		#Store $ra on element 4 of stack
+	sw		$a0, 0($sp)		#Store $a0 on element 0 of stack
+	sw		$a1, 4($sp)		#Store $a0 on element 0 of stack
+	sw		$a2, 8($sp)		#Store $a2 on element 2 of stack
+	
+	#HORIZONTAL LOOP
+	vertLoop:
+	jal		drawDot			#Jump and Link to drawDot
+	
+	#RESTORE REGISTERS
+	lw		$a0, 0($sp)		#Restore $a0 from stack
+	lw		$a1, 4($sp)		#Restore $a1 from stack
+	lw		$a2, 8($sp)		#Restore $a2 from stack
+	
+	#INCREMENT VALUES
+	addi		$a1, $a1, 1		#Increment y by 1
+	sw		$a1, 4($sp)		#Store $a1 on element 1 of stack
+	addi		$a3, $a3, -1		#Decrement length of line
+	bne		$a3, $0, vertLoop	#If length is not 0, loop
+	
+	#RESTORE $RA
+	lw		$ra, 12($sp)		#Restore $ra from stack
+	addi		$sp, $sp, 16		#Readjust stack
+	
+	jr		$ra			#Return
